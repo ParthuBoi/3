5c93a3d05b660fe049834201d5bd05a70a4f644c
ParthuBoi <100480418+ParthuBoi@users.noreply.github.com>
2022-11-27 16:48:33 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,45 @@
+Status = "";
+TV_image = "";
+objects = [];
+
+function preload(){
+    TV_image = loadImage("tv.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    object_Detector.detect(TV_image,gotResults);
+    
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(TV_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            console.log('rect drawn');
+        }
+    }
+}
